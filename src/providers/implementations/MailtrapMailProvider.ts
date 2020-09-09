786dd5d7d2c0bd31ad9e1ad25c9ef7d147f28ad2
Diego Rocha <diego.webdg@gmail.com>
2020-09-08 22:11:54 -0300

    init

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,34 @@
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+import { IMailProvider, IMessage } from '../IMailProvider';
+
+export class MailtrapMailProvider implements IMailProvider {
+    private transporter: Mail;
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.mailtrap.io',
+            port: 2525,
+            auth: {
+                user: '37428e922dc8ba',
+                pass: 'ab05d7badbc1bf',
+            }
+        })
+    }
+
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email,
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email,
+            },
+            subject: message.subject,
+            html: message.body,
+        })
+    }
+}
